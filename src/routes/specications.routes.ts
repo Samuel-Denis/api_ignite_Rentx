fd90d50a385dee50b75711801ce092b982fa0dc4
unknown <54408431+Samuel-Denis@users.noreply.github.com>
2021-10-29 20:44:20 -0300

    2.1
    correção de pasta

--- src/routes/specications.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import { createdCategoryController } from '../modules/cars/useCases/createdCategory';
-
-const specificationRoutes = Router();
-
-specificationRoutes.post('/', (req, res) => createdCategoryController.handle(req, res));
-
-export { specificationRoutes };
